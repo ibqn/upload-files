5a1ab59cd2c590eb4adec2ca7aa65b0c784da50c
Evgeny Bobkin <evgeny.bobkin@europace.de>
2025-07-02 09:46:13 +0200

    update zod import path and simplify error message mapping

--- packages/vite-upload/src/lib/handle-error.ts
+++ packages/vite-upload/src/lib/handle-error.ts
@@ -1,13 +1,11 @@
 import { toast } from 'sonner'
-import { z } from 'zod'
+import { z } from 'zod/v4'
 
 export function getErrorMessage(error: unknown) {
   const unknownError = 'Something went wrong, please try again later.'
 
   if (error instanceof z.ZodError) {
-    const errors = error.issues.map((issue) => {
-      return issue.message
-    })
+    const errors = error.issues.map(({ message }) => message)
     return errors.join('\n')
   } else if (error instanceof Error) {
     return error.message
